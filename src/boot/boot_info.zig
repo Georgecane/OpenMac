35206f673775623a238d56857a88c5f19b54a4cd
Georgecane <151010590+Georgecane@users.noreply.github.com>
2026-09-20 22:10:21 +0330

    boot: remove invalid post-exit memory-map key

--- src/boot/boot_info.zig
+++ src/boot/boot_info.zig
@@ -3,15 +3,6 @@ const uefi = @import("std").os.uefi;
 pub const BootInfo = struct {
     memory_map: []align(@alignOf(uefi.tables.MemoryDescriptor)) u8,
     memory_map_size: usize,
-    memory_map_key: uefi.tables.MemoryMapKey,
     memory_descriptor_size: usize,
     memory_descriptor_version: u32,
-};
-
-pub const MemoryMap = struct {
-    buffer: []align(@alignOf(uefi.tables.MemoryDescriptor)) u8,
-    size: usize,
-    descriptor_size: usize,
-    descriptor_version: u32,
-    key: uefi.tables.MemoryMapKey,
 };
