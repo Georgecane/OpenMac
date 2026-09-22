42d29a4bffa254b687c2eae32f595e6d91d5dd75
Georgecane <151010590+Georgecane@users.noreply.github.com>
2026-09-23 03:29:25 +0330

    Fix UEFI outputString error value handling

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const kernel = @import("kernel/kernel.zig");
 
 fn fatal() noreturn {
     const con_out = uefi.system_table.con_out orelse unreachable;
-    con_out.outputString(std.unicode.utf8ToUtf16LeStringLiteral("OpenMac boot failure.\r\n")) catch {};
+    _ = con_out.outputString(std.unicode.utf8ToUtf16LeStringLiteral("OpenMac boot failure.\r\n")) catch false;
     while (true) {
         asm volatile ("hlt");
     }
